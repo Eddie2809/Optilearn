c873ed6f27a57b01af1cde547d1a9e6b92f65ba7
Eddie2809 <eddiealejandrovargas@gmail.com>
2022-04-23 16:44:23 -0500

    adding minor features

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -8,6 +8,8 @@ export default class Calendar extends React.Component{
         let color
         let events = []
         let mapTopicIdName = new Map()
+        let today = new Date()
+
         for(let i = 0; i < this.props.topics.length; i++){
             date = new Date((this.props.topics[i].first_session_date).slice(0,10)+'T00:00:00')
             mapTopicIdName[this.props.topics[i].id] = this.props.topics[i].name
@@ -22,7 +24,8 @@ export default class Calendar extends React.Component{
             
         for(let i = 0; i < this.props.reviews.length; i++){
             date = new Date((this.props.reviews[i].review_date).slice(0,10)+'T00:00:00')
-            color = (this.props.reviews[i].done? "#4AC563":"#EBF144")
+            console.log(date.getTime() > today.getTime())
+            color = (this.props.reviews[i].done? "#4AC563":(date.getTime() > today.getTime()? "#EBF144": "#FF3737"))
             events.push({
                 title: mapTopicIdName[this.props.reviews[i].topic_id],
                 date: (date.getFullYear() + "-" + ((date.getMonth()+1)<10? ("0" + (date.getMonth()+1)):((date.getMonth()+1))) + "-" + ((date.getDate())<10? ("0" + (date.getDate())):((date.getDate())))),
@@ -37,6 +40,14 @@ export default class Calendar extends React.Component{
         this.loadEvents()
         return(
             <div className="calendar">
+                <div className='colors'>
+                    <ul>
+                        <li className="green">Repaso realizado</li>
+                        <li className="blue">Sesión de estudio inicial</li>
+                        <li className="yellow">Repaso por realizar</li>
+                        <li className='red'>Repaso no realizado</li>
+                    </ul>
+                </div>
                 <FullCalendar
                     height="100vh"
                     locale= "esLocale"
